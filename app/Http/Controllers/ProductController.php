2a5a4745e27d93e272d12af72d1ec66bf9d425ba
DevProsper <prosperngouari38@gmail.com>
2017-12-14 13:17:25 +0100

    Gestion du controller index, vue, et la route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+    	$products = Product::orderBy('created_at', 'desc')->paginate(3);
+    	return view('shop.index')->with('products', $products);
+    }
 }
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -2,74 +2,23 @@
 
 @section('content')
   <div class="row">
-    <div class="col-sm-6 col-xs-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="image">
-        <div class="caption">
-          <h3>Sweet Rose</h3>
-          <h2>35$</h2>
-          <p>Un super sweet rose</p>
-          <p>
-            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
-          </p>
-        </div>
-      </div>
-    </div>
 
-     <div class="col-sm-6 col-xs-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="image">
-        <div class="caption">
-          <h3>Sweet Rose</h3>
-          <h2>35$</h2>
-          <p>Un super sweet rose</p>
-          <p>
-            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
-          </p>
+    @foreach($products as $product)
+      <div class="col-sm-6 col-xs-12 col-md-4">
+        <div class="thumbnail">
+          <img src="{{$product->image}}" alt="image">
+          <div class="caption">
+            <h3>{{$product->title}}</h3>
+            <h2>{{$product->price}}$</h2>
+            <p>{{$product->description}}</p>
+            <p>
+              <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    @endforeach
 
-     <div class="col-sm-6 col-xs-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="image">
-        <div class="caption">
-          <h3>Sweet Rose</h3>
-          <h2>35$</h2>
-          <p>Un super sweet rose</p>
-          <p>
-            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
-          </p>
-        </div>
-      </div>
-    </div>
-
-     <div class="col-sm-6 col-xs-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="image">
-        <div class="caption">
-          <h3>Sweet Rose</h3>
-          <h2>35$</h2>
-          <p>Un super sweet rose</p>
-          <p>
-            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
-          </p>
-        </div>
-      </div>
-    </div>
-
-     <div class="col-sm-6 col-xs-12 col-md-4">
-      <div class="thumbnail">
-        <img src="..." alt="image">
-        <div class="caption">
-          <h3>Sweet Rose</h3>
-          <h2>35$</h2>
-          <p>Un super sweet rose</p>
-          <p>
-            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
-          </p>
-        </div>
-      </div>
-    </div>
   </div>
+  {{$products->links()}}
 @endsection
