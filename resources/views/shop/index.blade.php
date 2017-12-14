91744c1c6bfadbb2adedb8130f4d171010e95775
DevProsper <prosperngouari38@gmail.com>
2017-12-14 11:37:32 +0100

    Mise place des styles

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.layout')
+
+@section('content')
+  <div class="row">
+    <div class="col-sm-6 col-xs-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="image">
+        <div class="caption">
+          <h3>Sweet Rose</h3>
+          <h2>35$</h2>
+          <p>Un super sweet rose</p>
+          <p>
+            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+          </p>
+        </div>
+      </div>
+    </div>
+
+     <div class="col-sm-6 col-xs-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="image">
+        <div class="caption">
+          <h3>Sweet Rose</h3>
+          <h2>35$</h2>
+          <p>Un super sweet rose</p>
+          <p>
+            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+          </p>
+        </div>
+      </div>
+    </div>
+
+     <div class="col-sm-6 col-xs-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="image">
+        <div class="caption">
+          <h3>Sweet Rose</h3>
+          <h2>35$</h2>
+          <p>Un super sweet rose</p>
+          <p>
+            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+          </p>
+        </div>
+      </div>
+    </div>
+
+     <div class="col-sm-6 col-xs-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="image">
+        <div class="caption">
+          <h3>Sweet Rose</h3>
+          <h2>35$</h2>
+          <p>Un super sweet rose</p>
+          <p>
+            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+          </p>
+        </div>
+      </div>
+    </div>
+
+     <div class="col-sm-6 col-xs-12 col-md-4">
+      <div class="thumbnail">
+        <img src="..." alt="image">
+        <div class="caption">
+          <h3>Sweet Rose</h3>
+          <h2>35$</h2>
+          <p>Un super sweet rose</p>
+          <p>
+            <a href="#" class="btn btn-primary" role="button">Ajouter au panier</a> 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
